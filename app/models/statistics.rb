23dd93d0f212639f640c3fae1b66bce2b4c83a03
powder-river <dadkisso@live.com>
2016-09-21 20:15:40 -0400

    add passing

--- app/models/statistics.rb
+++ app/models/statistics.rb
@@ -3,6 +3,12 @@ class Statistics
   def initialize
   end
 
+  def nfl_data
+    receiving
+    rushing
+    passing
+  end
+
   def receiving
     doc = Nokogiri::HTML(open("http://www.pro-football-reference.com/years/2016/receiving.htm"))
     wrs = doc.xpath("//table/tbody/tr")
@@ -41,21 +47,50 @@ class Statistics
       row << ["name","team","age","pos","games","gamesStarted","touches","yards","touchdowns","longRush","yardsPerCarry","yardsPerGame","touchPerGame","fumbles"]
       rush.each do |rush|[1]
         rush = rush.children
-        row << [
-        rush[1].content.upcase, #name
-        rush[2].content, #team
-        rush[3].content, #age
-        rush[4].content.upcase, #position
-        rush[5].content, #games
-        rush[6].content, #started
-        rush[7].content, #touches
-        rush[8].content, #yards
-        rush[9].content, #touchdowns
-        rush[10].content, #longest rush
-        rush[11].content, #yards per carry
-        rush[12].content, #yards per game
-        rush[13].content, #touches per game
-        rush[25].content] #fumbles
+        unless rush[1].content == "Rk"
+          row << [
+          rush[1].content.upcase, #name
+          rush[2].content, #team
+          rush[3].content, #age
+          rush[4].content.upcase, #position
+          rush[5].content, #games
+          rush[6].content, #started
+          rush[7].content, #touches
+          rush[8].content, #yards
+          rush[9].content, #touchdowns
+          rush[10].content, #longest rush
+          rush[11].content, #yards per carry
+          rush[12].content, #yards per game
+          rush[13].content, #touches per game
+          rush[25].content] #fumbles
+        end
+      end
+    end
+  end
+
+
+  def passing
+    doc = Nokogiri::HTML(open("http://www.pro-football-reference.com/years/2016/passing.htm"))
+    pass = doc.xpath("//table/tbody/tr")
+    CSV.open("./app/models/data/passing.csv", "wb") do |row|
+      row << ["name","team","age","pos","games","gamesStarted","completions","attempts","comPercent","yards","touchdowns","yardsPerGame"]
+      pass.each do |ps|[1]
+        ps = ps.children
+        unless ps[1].content == "Rk"
+          row << [
+          ps[1].content.upcase, #name
+          ps[2].content, #team
+          ps[3].content, #age
+          ps[4].content.upcase, #position
+          ps[5].content, #games
+          ps[6].content, #started
+          ps[8].content, #completions
+          ps[9].content, #attempts
+          ps[10].content, #completion percent
+          ps[11].content, #yards
+          ps[12].content, #touchdowns
+          ps[20].content] #yards per game
+        end
       end
     end
   end
